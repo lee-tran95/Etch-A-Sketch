d81525f83cb942c36a3925ce66dd4fa2810f037d
Lee Tran <lee.tran95@yahoo.com>
2022-06-05 21:07:02 -0700

    Add mouse over events to each sqaure to change the color

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-for(let i = 0; i < 4; i++){
+
+for(let i = 0; i < 256; i++){
     let square = document.createElement('div');
     square.classList.add('square')
+    square.setAttribute('onmousedown', 'return false')
     document.querySelector('.outter').appendChild(square)
 }
+
+let squares = document.querySelectorAll('.square');
+
+let mousedown = false;
+
+document.addEventListener('mousedown', () => {
+    mousedown = true;
+})
+document.addEventListener('mouseup', () => {
+    mousedown = false;
+})
+
+squares.forEach(square => {
+    square.addEventListener('mouseenter',() => {
+        if(mousedown){
+            square.style.background = "black";
+        }
+    })
+})
+
+
