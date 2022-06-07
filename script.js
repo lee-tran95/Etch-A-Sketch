36da15708b2947f28e13acf1318a909a011d714e
Lee Tran <lee.tran95@yahoo.com>
2022-06-06 17:02:39 -0700

    Implement a button that prompts user for the number of squares per side

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 
-for(let i = 0; i < 256; i++){
-    let square = document.createElement('div');
-    square.classList.add('square')
-    square.setAttribute('onmousedown', 'return false')
-    document.querySelector('.outter').appendChild(square)
+let promptButton = document.querySelector('.numSquare');
+
+promptButton.addEventListener('click', createGrid);
+
+let numberOfSquare = 0;
+
+let grid = document.querySelector('.outter');
+
+
+function createGrid(){
+    numberOfSquare = prompt("Enter the number of square for each side")
+    console.log(numberOfSquare)
+    for(let i = 0; i < numberOfSquare; i++){
+        let square = document.createElement('div');
+        square.classList.add('square')
+        square.setAttribute('onmousedown', 'return false')
+        grid.appendChild(square)
+    }
+    let squares = document.querySelectorAll('.square');
+    let mousedown = false;
+
+    document.addEventListener('mousedown', () => {
+        mousedown = true;
+    })
+    document.addEventListener('mouseup', () => {
+        mousedown = false;
+    })
+
+    squares.forEach(square => {
+        square.addEventListener('mouseenter',() => {
+            if(mousedown){
+                square.style.background = "black";
+            }
+        })
+    })
 }
 
-let squares = document.querySelectorAll('.square');
-
-let mousedown = false;
-
-document.addEventListener('mousedown', () => {
-    mousedown = true;
-})
-document.addEventListener('mouseup', () => {
-    mousedown = false;
-})
-
-squares.forEach(square => {
-    square.addEventListener('mouseenter',() => {
-        if(mousedown){
-            square.style.background = "black";
-        }
-    })
-})
 
 
